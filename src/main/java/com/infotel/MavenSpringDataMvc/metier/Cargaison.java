e20687013b1418e1afdbc5d139b9351c7aa2e06f
IN-MP-028 <javajee2k19@gmail.com>
2019-05-06 11:11:02 +0200

    push class cargaison v1

---
+++ src/main/java/com/infotel/MavenSpringDataMvc/metier/Cargaison.java
@@ -0,0 +1,52 @@
+package com.infotel.MavenSpringDataMvc.metier;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+
+
+public abstract class Cargaison {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idCargaison;
+
+	protected String nomCargaison;
+	protected String depart;
+	protected String destination;
+	
+	//@ManyToOne
+	//private SocieteTransport societeTransport;
+	
+	
+	public int getIdCargaison() {
+		return idCargaison;
+	}
+	public void setIdCargaison(int idCargaison) {
+		this.idCargaison = idCargaison;
+	}
+	public String getNomCargaison() {
+		return nomCargaison;
+	}
+	public void setNomCargaison(String nomCargaison) {
+		this.nomCargaison = nomCargaison;
+	}
+	public String getDepart() {
+		return depart;
+	}
+	public void setDepart(String depart) {
+		this.depart = depart;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+	
+	
+	
+	
+}
