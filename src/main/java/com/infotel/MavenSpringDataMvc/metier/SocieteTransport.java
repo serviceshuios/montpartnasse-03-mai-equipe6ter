d5a2a4f2a685e657b2488e09caf74e022af8e195
IN-MP-029 <sen.95@hotmail.fr>
2019-05-06 11:12:04 +0200

    societeTransport V1

---
+++ src/main/java/com/infotel/MavenSpringDataMvc/metier/SocieteTransport.java
@@ -0,0 +1,52 @@
+package com.infotel.MavenSpringDataMvc.metier;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Entity
+@Scope
+@Component
+public class SocieteTransport {
+	
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+
+private int idSociete;
+private String nomSociete;
+private String numSiret;
+private Date dateCreation;
+public int getIdSociete() {
+	return idSociete;
+}
+public void setIdSociete(int idSociete) {
+	this.idSociete = idSociete;
+}
+public String getNomSociete() {
+	return nomSociete;
+}
+public void setNomSociete(String nomSociete) {
+	this.nomSociete = nomSociete;
+}
+public String getNumSiret() {
+	return numSiret;
+}
+public void setNumSiret(String numSiret) {
+	this.numSiret = numSiret;
+}
+public Date getDateCreation() {
+	return dateCreation;
+}
+public void setDateCreation(Date dateCreation) {
+	this.dateCreation = dateCreation;
+}
+
+
+
+}
