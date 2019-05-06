5469996e69fd676214d76b91c1ab9164e4af68fb
IN-MP-029 <sen.95@hotmail.fr>
2019-05-06 11:16:38 +0200

    class Societe transport

--- src/main/java/com/infotel/MavenSpringDataMvc/metier/SocieteTransport.java
+++ src/main/java/com/infotel/MavenSpringDataMvc/metier/SocieteTransport.java
@@ -8,7 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Component; 
 
 @Entity
 @Scope
